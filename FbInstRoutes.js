bbc709d73d6fb97499ab279a6eb849d3a845a263
shubham ralli <sralli73@gmail.com>
2024-04-06 17:26:22 +0530

    file add 3

--- FbInstRoutes.js
+++ FbInstRoutes.js
@@ -13,7 +13,7 @@ router.post('/getVideoUrls', async (req, res) => {
         const videoUrls = await ndown(url);
         res.json({ success: true, videoUrls: videoUrls });
 
-        console.log(videoUrls);
+        // console.log(videoUrls);
     } catch (error) {
         res.json({ success: false, message: 'Error retrieving video URLs.' });
     }

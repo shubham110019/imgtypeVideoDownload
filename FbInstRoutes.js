262531f8e7f77ad029245ad4afd1048acbc0883c
shubham ralli <sralli73@gmail.com>
2024-04-06 17:07:54 +0530

    first commit

---
+++ FbInstRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const { ndown } = require('nayan-media-downloader');
+const bodyParser = require('body-parser');
+
+router.use(cors());
+router.use(bodyParser.json());
+
+router.post('/getVideoUrls', async (req, res) => {
+    const url = req.body.urls[0];
+    try {
+        const videoUrls = await ndown(url);
+        res.json({ success: true, videoUrls: videoUrls });
+    } catch (error) {
+        res.json({ success: false, message: 'Error retrieving video URLs.' });
+    }
+});
+
+module.exports = router;
